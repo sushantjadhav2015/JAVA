41f43efd62ea8a2308d6923fbf03d1e854a01c2e
sushant jadhav <sushantjadhav2015@gmail.com>
2024-02-03 21:03:47 +0530

    constructor added

---
+++ Constructor/src/defaults/FirstConstructor.java
@@ -0,0 +1,14 @@
+package defaults;
+
+public class FirstConstructor {
+
+	public FirstConstructor() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
