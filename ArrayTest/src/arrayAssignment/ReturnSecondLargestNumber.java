81ac7dfd7866bfe50aebabcd5c69835d0263a5e9
sushant jadhav <sushantjadhav2015@gmail.com>
2024-02-04 17:51:46 +0530

    count assignment solved

---
+++ ArrayTest/src/arrayAssignment/ReturnSecondLargestNumber.java
@@ -0,0 +1,28 @@
+package arrayAssignment;
+
+import java.util.Arrays;
+
+public class ReturnSecondLargestNumber {
+
+	public static void main(String[] args) {
+		int[] arr = {25,10,35,15,45};
+
+		ReturnSecondLargestNumber Obj = new ReturnSecondLargestNumber();
+		int num = Obj.secondLargNumber(arr);
+		System.out.println("Second largest number is : "+num);
+	}
+
+	static int secondLargNumber(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length-1; j++) {
+				if (arr[j]>arr[j+1]) {
+					int temp = arr[j+1];
+					arr[j+1] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		System.out.println("array in ascending order :"+Arrays.toString(arr));
+		return arr[1];
+	}
+}
